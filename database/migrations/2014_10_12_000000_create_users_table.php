6ca3c6e8597e5441baab1bae6ac74e80c016b3f8
mahsamabbas <syedmahsam@gmail.com>
2023-01-09 23:34:30 +0500

    db fixes

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::dropIfExists('users');
+        Schema::dropIfExists('personal_access_tokens');
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
