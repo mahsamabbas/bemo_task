5c86eed48177c4a509e64b92a5064624e233a121
mahsamabbas <syedmahsam@gmail.com>
2023-01-11 16:00:03 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/db-dump', function () {
-
     \Spatie\DbDumper\Databases\MySql::create()
         ->setDbName(config('database.connections.mysql.database'))
         ->setUserName(config('database.connections.mysql.username'))
