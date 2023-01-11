7a0ad2ee88c2c28229dd30d29f9608f591608303
mahsamabbas <syedmahsam@gmail.com>
2023-01-11 15:42:42 +0500

    dump config

--- config/database.php
+++ config/database.php
@@ -61,11 +61,11 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
-            'dump' => [
-                'dump_binary_path' => '/app/.heroku/vendor/mysql/bin',
-                'use_single_transaction',
-                'timeout' => 60 * 5, // 5 minute timeout
-            ],
+//            'dump' => [
+//                'dump_binary_path' => '/app/.heroku/vendor/mysql/bin',
+//                'use_single_transaction',
+//                'timeout' => 60 * 5, // 5 minute timeout
+//            ],
         ],
 
         'pgsql' => [
